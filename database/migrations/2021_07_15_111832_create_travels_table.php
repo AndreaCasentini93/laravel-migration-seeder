cd5c563888bc829c184e343c7c25022304b11824
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-07-15 14:14:03 +0200

    Migrations

--- database/migrations/2021_07_15_111832_create_travels_table.php
+++ database/migrations/2021_07_15_111832_create_travels_table.php
@@ -15,6 +15,16 @@ class CreateTravelsTable extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('image', 200);
+            $table->string('location', 20);
+            $table->tinyInteger('stars');
+            $table->string('address', 50);
+            $table->tinyInteger('vote');
+            $table->boolean('free_cancellation');
+            $table->string('room', 100);
+            $table->string('board', 20);
+            $table->string('price', 10);
             $table->timestamps();
         });
     }
